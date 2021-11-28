877f96771217df0352fb944a076d79382947c7cb
Peter Rúček <rucekpeter2000@hotmail.com>
2021-11-28 20:54:33 +0100

    Radically changing view, view is not model based now + Button styling + Removed unnecessary buttons

--- Controllers/MainWindowController.py
+++ Controllers/MainWindowController.py
@@ -3,76 +3,51 @@
 #   @Authors Marek Miček (xmicek08), ....
 #   @date 5.11.2021
 
-from ITU.Views.MainWindow import *
+from Views.MainWindow import *
+from Views.LessonDetailView import *
+from Models.LessonRepository import LessonRepository
+from Models.CardRepository import CardRepository
+from Models.CollectionRepository import CollectionRepository
+from functools import partial
 
 
 class MainWindowController:
 
-    def __init__(self, model, view):
-        """Store instances of MainWindow model and view to controller."""
-        self._model = model
+    def __init__(self, view):
+        """Store instances of repositories and  view to controller."""
         self._view = view
-        self.add_dialog = None
+        self.collection_repo = CollectionRepository()
+        self.lesson_repo = LessonRepository()
+        self.card_repo = CardRepository()
+
+        self.stacked_widget = QStackedWidget()
+        self.stacked_widget.addWidget(self._view)
+        self.stacked_widget.show()
+
+        lessons = self.lesson_repo.get_all_lessons()
+        lessonButtons = []
+        index = 0
+
+        self._view.grid.addWidget(self._view.addButton, 0, 0)
+        for i in range(len(lessons)):
+            r = int((4 % (i + 1)) / 4)
+            self._view.grid.setColumnStretch(i % 4, 1)
+            self._view.grid.setRowStretch(r, 1)
+            lessonButtons.append(QPushButton(lessons[i].name))
+            lessonButtons[index].setStyleSheet(self._view.button_style_sheet)
+            if r == 0:
+                self._view.grid.addWidget(lessonButtons[index], 0, i + 1)
+            else:
+                self._view.grid.addWidget(lessonButtons[index], r, i % 4)
+
+            lessonButtons[index].clicked.connect(partial(self.add_lesson_view, lessons[i].name, lessons[i].study_field))
+            index = index + 1
 
         """Connect signals from view to particular slots in controller."""
-        self._view.addButton.clicked.connect(self.Add_Lesson_Dialog)
-        self._view.deleteButton.clicked.connect(self.Delete_Lesson)
-        self._view.clearAllButton.clicked.connect(self.Delete_All_Lessons)
+        self._view.addButton.clicked.connect(self.add_lesson_view)
 
-    def Add_Lesson_Dialog(self):
-        """Create instance of dialog window for adding new lessons."""
-        self.add_dialog = Add_Lesson_DialogGUI(self._view)
+    def add_lesson_view(self, lesson_name, study_field):
+        self.lessonDetailView = LessonDetailView(lesson_name, study_field)
 
-        """Connect "OK" and "Cancel" signals from dialog window to slots."""
-        self.add_dialog.buttonsBox.accepted.connect(self.Accept_Lesson)
-        self.add_dialog.buttonsBox.rejected.connect(self.add_dialog.reject)   # rejection is made implicitly by QT
-        self.add_dialog.exec()
-
-    def Accept_Lesson(self):
-        """Accpets data provided through form in dialog window and add new lesson."""
-        self.add_dialog.data = []
-        for form_field in (self.add_dialog.lesson_name, self.add_dialog.lesson_field):
-            if not form_field.text():   # check if all fields of form are filled
-                QMessageBox.critical(
-                    self.add_dialog,
-                    "Error!",
-                    f"You must provide a lesson's {form_field.objectName()}",
-                )
-                self.add_dialog.data = None     # clear data in case of error
-                return
-
-            """Store text fields from dialog."""
-            self.add_dialog.data.append(form_field.text())
-
-        self.add_dialog.accept()    # close dialog window after user's form was accepted
-
-        """Store dialog input to main view model and update main view."""
-        self._model.Add_Lesson(self.add_dialog.data)
-
-    def Delete_Lesson(self):
-        """Delete selected lesson from view and DB"""
-        row = self._view.table.currentIndex().row()     # get selected row
-        if row < 0:
-            return
-
-        warning = QMessageBox.warning(
-            self._view,
-            "Warning !",
-            "Do you want to delete selected lesson with the whole progress entirely ?",
-            QMessageBox.Ok | QMessageBox.Cancel
-        )
-
-        if warning == QMessageBox.Ok:
-            self._model.Delete_Lesson(row)
-
-    def Delete_All_Lessons(self):
-        """Delete all lesson from view and DB"""
-        warning = QMessageBox.warning(
-            self._view,
-            "Warning !",
-            "Do you want to delete all lessons with the whole progress entirely ?",
-            QMessageBox.Ok | QMessageBox.Cancel
-        )
-
-        if warning == QMessageBox.Ok:
-            self._model.Clear_Lessons()
+        self.stacked_widget.addWidget(self.lessonDetailView)
+        self.stacked_widget.setCurrentIndex(self.stacked_widget.currentIndex()+1)
--- Views/MainWindow.py
+++ Views/MainWindow.py
@@ -1,48 +1,27 @@
-##  Views/MainWindow.py module
-#   Implements the UI for the main view of app
-#   @Authors Marek Miček (xmicek08), ....
-#   @date 5.11.2021
-
-from ITU.Views.UI_Designer.MainWindow import Ui_MainWindow
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from PyQt5 import QtCore
 
 
 class MainWindow(QMainWindow):
-    """Main Window."""
 
-    def __init__(self, model, parent=None):
-        """Initializer."""
+    def __init__(self, parent=None):
         super().__init__(parent)
-        """
-            Príklad použita zkompilovaného .UI súboru z QT designeru
-            Vytvoria sa inštancie na triedy v module Views/UI_Designer
-            Inštancie sa môžu robiť na celé views alebo len widgety
-            No vygenrovany kód je neprehľadný a neefektivny, tak som radšej
-            nepužíval tento spôsob...no do budúcna aspoň vieme ako
-            
-            UI_MainWin = Ui_MainWindow()
-            UI_MainWin.setupUi(self)
-        """
 
         self.setWindowTitle("StudyDex")
         self.resize(800, 600)
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
-        self.layout = QHBoxLayout()
+        self.layout = QVBoxLayout()
+        self.grid = QGridLayout()
         self.centralWidget.setLayout(self.layout)
-        self.MainViewModel = model
 
-        self.setupUI()
+        self.setup_ui()
 
-    def setupUI(self):
-        """Setup the main window's GUI."""
-        # Create the table view widget
-        self.table = QTableView()
-        self.table.setModel(self.MainViewModel.model)
-        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+    def setup_ui(self):
+
+        self.grid.setContentsMargins(50, 40, 50, 20)
 
         # set font for buttons
         button_font = QFont()
@@ -50,23 +29,30 @@ class MainWindow(QMainWindow):
         button_font.setPointSize(15)
         button_font.setBold(False)
         button_font.setWeight(50)
+        self.button_style_sheet = \
+            "QPushButton {" \
+            "background-color:qlineargradient(x1:0, y1:0, x2:1, y2:1,stop:0 white, stop: 0.8 orange, stop:1 white);" \
+            "border-style: outset;" \
+            "border-width: 2px;" \
+            "border-radius: 10px;" \
+            "border-color: gray;" \
+            "font: bold 14px;" \
+            "min-width: 10em;" \
+            "padding: 50px 20px; " \
+            "}" \
+            "QPushButton:pressed { " \
+            "border-style: inset;" \
+            "}" \
+            "QPushButton:hover { " \
+            "background-color: orange;" \
+            "}"
 
         # Create buttons
         self.addButton = QPushButton("Add lesson")
-        self.addButton.setStyleSheet("background-color: #729FCF")
         self.addButton.setFont(button_font)
-
-        self.deleteButton = QPushButton("Delete lesson")
-        self.deleteButton.setStyleSheet("background-color: #729FCF")
-        self.deleteButton.setFont(button_font)
-
-        self.enterButton = QPushButton("Enter lesson")
-        self.enterButton.setStyleSheet("background-color: #729FCF")
-        self.enterButton.setFont(button_font)
-
-        self.clearAllButton = QPushButton("Clear All")
-        self.clearAllButton.setStyleSheet("background-color: #729FCF")
-        self.clearAllButton.setFont(button_font)
+        self.addButton.setStyleSheet(self.button_style_sheet)
+        self.addButton.setIcon(QIcon(r'Views\Assets\plus.png'))
+        self.addButton.setIconSize(QtCore.QSize(50, 50))
 
         # set font for header
         header_font = QFont()
@@ -81,53 +67,5 @@ class MainWindow(QMainWindow):
         self.label.setAlignment(Qt.AlignCenter)
         self.label.setFont(header_font)
 
-        # Lay out buttons of the MainWindow
-        button_layout = QVBoxLayout()
-        button_layout.addWidget(self.addButton)
-        button_layout.addWidget(self.deleteButton)
-        button_layout.addWidget(self.enterButton)
-        button_layout.addStretch()
-        button_layout.addWidget(self.clearAllButton)
-
-        # Lay out table and header of the MainWindow
-        table_layout = QVBoxLayout()
-        table_layout.addWidget(self.label)
-        table_layout.addWidget(self.table)
-
-        # add both layouts to global layout
-        self.layout.addLayout(table_layout)
-        self.layout.addLayout(button_layout)
-
-class Add_Lesson_DialogGUI(QDialog):
-    """"GUI of dialog window for entering new lesson."""
-
-    def __init__(self, parent=None):
-        super().__init__(parent=parent)
-        self.setWindowTitle("Add new Lesson")
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-        self.data = None
-
-        self.setupUI()
-
-    def setupUI(self):
-        """Setup the Add Lesson dialog's GUI."""
-        # Create line edits for data fields
-        self.lesson_name = QLineEdit()
-        self.lesson_name.setObjectName("Name")
-        self.lesson_field = QLineEdit()
-        self.lesson_field.setObjectName("Study field")
-
-        # Lay out the data fields
-        layout = QFormLayout()
-        layout.addRow("Name:", self.lesson_name)
-        layout.addRow("Study field:", self.lesson_field)
-        self.layout.addLayout(layout)
-
-        # Add standard buttons to the dialog (Ok/Cancel)
-        self.buttonsBox = QDialogButtonBox(self)
-        self.buttonsBox.setOrientation(Qt.Horizontal)
-        self.buttonsBox.setStandardButtons(
-            QDialogButtonBox.Ok | QDialogButtonBox.Cancel
-        )
-        self.layout.addWidget(self.buttonsBox)
+        self.layout.addWidget(self.label)
+        self.layout.addLayout(self.grid)
