7a39c75cead88087ede10adb607a5ca1a5363791
Peter Rúček <rucekpeter2000@hotmail.com>
2021-11-30 14:32:01 +0100

    Added templates - button styling is a separate file

--- Views/MainWindow.py
+++ Views/MainWindow.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5 import QtCore
+from Views.Templates.ButtonStyling import BUTTON_STYLING
 
 
 class MainWindow(QMainWindow):
@@ -9,13 +10,14 @@ class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.setWindowTitle("StudyDex")
-        self.resize(800, 600)
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
         self.layout = QVBoxLayout()
         self.grid = QGridLayout()
         self.centralWidget.setLayout(self.layout)
+
+        self.addButton = QPushButton("Add lesson")
+        self.label = QLabel()
 
         self.setup_ui()
 
@@ -29,26 +31,10 @@ class MainWindow(QMainWindow):
         button_font.setPointSize(15)
         button_font.setBold(False)
         button_font.setWeight(50)
-        self.button_style_sheet = \
-            "QPushButton {" \
-            "background-color:qlineargradient(x1:0, y1:0, x2:1, y2:1,stop:0 white, stop: 0.8 orange, stop:1 white);" \
-            "border-style: outset;" \
-            "border-width: 2px;" \
-            "border-radius: 10px;" \
-            "border-color: gray;" \
-            "font: bold 14px;" \
-            "min-width: 10em;" \
-            "padding: 50px 20px; " \
-            "}" \
-            "QPushButton:pressed { " \
-            "border-style: inset;" \
-            "}" \
-            "QPushButton:hover { " \
-            "background-color: orange;" \
-            "}"
+        self.button_style_sheet = BUTTON_STYLING
+
 
         # Create buttons
-        self.addButton = QPushButton("Add lesson")
         self.addButton.setFont(button_font)
         self.addButton.setStyleSheet(self.button_style_sheet)
         self.addButton.setIcon(QIcon(r'Views\Assets\plus.png'))
@@ -62,7 +48,6 @@ class MainWindow(QMainWindow):
         header_font.setWeight(50)
 
         # MainWindow header
-        self.label = QLabel()
         self.label.setText("StudyDex")
         self.label.setAlignment(Qt.AlignCenter)
         self.label.setFont(header_font)
---
+++ Views/Templates/ButtonStyling.py
@@ -0,0 +1,16 @@
+BUTTON_STYLING = "QPushButton {" \
+            "background-color:qlineargradient(x1:0, y1:0, x2:1, y2:1,stop:0 white, stop: 0.8 orange, stop:1 white);" \
+            "border-style: outset;" \
+            "border-width: 2px;" \
+            "border-radius: 10px;" \
+            "border-color: gray;" \
+            "font: bold 14px;" \
+            "min-width: 10em;" \
+            "padding: 50px 20px; " \
+            "}" \
+            "QPushButton:pressed { " \
+            "border-style: inset;" \
+            "}" \
+            "QPushButton:hover { " \
+            "background-color: orange;" \
+            "}"
