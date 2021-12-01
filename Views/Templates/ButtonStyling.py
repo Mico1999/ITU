c0ea133dbe25cea432355289f7f683ef0fe8f545
Peter Rúček <rucekpeter2000@hotmail.com>
2021-12-02 00:11:13 +0100

    Added more styling

--- Views/Templates/ButtonStyling.py
+++ Views/Templates/ButtonStyling.py
@@ -31,3 +31,36 @@ HOVER_BUTTON = "QPushButton {" \
             "QPushButton:hover { " \
             "border-style: inset;" \
             "}"
+
+WRONG_BUTTON = "QPushButton {" \
+            "background-color:qlineargradient(x1:0, y1:0, x2:1, y2:1,stop:0 white, stop: 0.8 red, stop:1 white);" \
+            "border-style: outset;" \
+            "border-width: 2px;" \
+            "border-radius: 10px;" \
+            "border-color: gray;" \
+            "font: bold 14px;" \
+            "min-width: 10em;" \
+            "padding: 50px 20px; " \
+            "}" \
+            "QPushButton:pressed { " \
+            "border-style: inset;" \
+            "}" \
+            "QPushButton:hover { " \
+            "background-color: darkred;" \
+            "}"
+RIGHT_BUTTON = "QPushButton {" \
+            "background-color:qlineargradient(x1:0, y1:0, x2:1, y2:1,stop:0 white, stop: 0.8 green, stop:1 white);" \
+            "border-style: outset;" \
+            "border-width: 2px;" \
+            "border-radius: 10px;" \
+            "border-color: gray;" \
+            "font: bold 14px;" \
+            "min-width: 10em;" \
+            "padding: 50px 20px; " \
+            "}" \
+            "QPushButton:pressed { " \
+            "border-style: inset;" \
+            "}" \
+            "QPushButton:hover { " \
+            "background-color: darkgreen;" \
+            "}"
